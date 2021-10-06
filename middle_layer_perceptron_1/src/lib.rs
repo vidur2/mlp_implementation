5c486d36fa00b2570d5dd84d3598990e3f9945fd
vidur2 <vmod2005@gmail.com>
2021-10-06 11:33:09 -0400

    finished middle layer nueron

--- middle_layer_perceptron_1/src/lib.rs
+++ middle_layer_perceptron_1/src/lib.rs
@@ -31,6 +31,10 @@ pub trait PerceptronWeights{
     fn predict(&self, input1: f32, input2: f32, mut outputs: Vec<f32>, mut input_vector: Vec<Vec<f32>>, expected_ouput: f32);
 }
 
+#[ext_contract(lower_level_nueron)]
+pub trait InputNueron2{
+    fn adjust(&mut self, offset: f32, input1: f32, input2: f32, input3: f32, input4: f32, input5: f32);
+}
 
 #[near_bindgen]
 impl MiddleNeuron{
@@ -49,6 +53,15 @@ impl MiddleNeuron{
         let higher_level_nueron_account_id: AccountId = HIGHER_LEVEL_NUERON_ID.to_string().trim().parse().expect("invalid");
         let gas_count = Gas::from(BASE_GAS);
         higher_level_nueron::predict(input1, input2, outputs, input_vector, expected_ouput,higher_level_nueron_account_id, NO_DEPOSIT, gas_count);
+    }
+    fn adjust(&mut self, offset: f32, input1: f32, input2: f32, input_vector: Vec<Vec<f32>>){
+        self.weight1 = self.weight1 + offset * input1;
+        self.weight2 = self.weight2 + offset * input2;
+        self.bias = self.bias + offset;
+        let lower_level_nueron_account_id: AccountId = LOWER_LEVEL_NUERON_ID.to_string().trim().parse().expect("invalid");
+        let gas_count = Gas::from(BASE_GAS);
+        lower_level_nueron::adjust(offset, input_vector[0][0], input_vector[0][1], input_vector[0][2], input_vector[0][3], input_vector[0][4], lower_level_nueron_account_id, NO_DEPOSIT, gas_count);
+
     }
     fn sigmoid(&self, input_sum: f32) -> f32{
         1f32/(1f32 + E.powf(-input_sum as f64) as f32)
