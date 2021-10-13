f969758d0011d309bcebef8a4665afe66e3522c1
vidur2 <vmod2005@gmail.com>
2021-10-13 18:54:15 -0400

    Final alpha stage

--- output_layer_perceptron/src/lib.rs
+++ output_layer_perceptron/src/lib.rs
@@ -6,6 +6,15 @@ Output Neuron
 use near_sdk::{near_bindgen, env, ext_contract, Gas, Balance, AccountId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 
+
+// Contract account of lower leve neuron
+const LOWER_LEVEL_NEURON_ID: &str = "mlp5.perceptron.testnet";
+
+// General Constants
+const NO_DEPOSIT: Balance = 0;
+const BASE_GAS: u64 = 100000000;
+
+
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, Default)]
 pub struct OutputNeuron{
@@ -13,6 +22,11 @@ pub struct OutputNeuron{
     weight2: f32,
     weight3: f32,
     bias: f32,
+}
+
+#[ext_contract(lower_level_neuron)]
+pub trait MiddleNeuron3{
+    fn adjust(&mut self, offset: f32, mut input_vector: Vec<Vec<f32>>);
 }
 
 #[near_bindgen]
@@ -29,13 +43,16 @@ impl OutputNeuron {
     pub fn predict(&mut self, input1: f32, input2: f32, input3: f32, input_vector: Vec<Vec<f32>>, expected_output: f32){
         let weighted_sum: f32 = input1 * self.weight1 + input2 * self.weight2 + input3 * self.weight3 + self.bias;
         let offset = expected_output - self.step_function(weighted_sum);
-        let input_vector = self.train(offset, input_vector);
+        let returned_values = self.train(offset, input_vector);
+        let lower_level_neuron_account_id: AccountId = LOWER_LEVEL_NEURON_ID.to_string().trim().parse().expect("invalid");
+        let gas_count = Gas::from(BASE_GAS);
+        lower_level_neuron::adjust(returned_values.1, returned_values.0, lower_level_neuron_account_id, NO_DEPOSIT, gas_count);
     }
-    fn train(&mut self, offset: f32, input_vector: Vec<Vec<f32>>) -> Vec<Vec<f32>>{
+    fn train(&mut self, offset: f32, input_vector: Vec<Vec<f32>>) -> (Vec<Vec<f32>>, f32){
         self.weight1 = self.weight1 + input_vector[2][0] * offset;
         self.weight2 = self.weight2 + input_vector[2][1] * offset;
         self.weight3 = self.weight3 + input_vector[2][2] * offset;
-        input_vector
+        (input_vector, offset)
     }
     fn step_function(&self, input_sum: f32) -> f32{
         if input_sum >= 0f32 {return 1f32}
