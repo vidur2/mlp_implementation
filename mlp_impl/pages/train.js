fdc962e221b8b573364d3db060b5a865c8334f4c
vidur2 <vmod2005@gmail.com>
2021-11-02 18:32:53 -0400

    updated commit

---
+++ mlp_impl/pages/train.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css"
+import Head from "next/head"
+import { connect, Contract, keyStores, KeyPair } from "near-api-js"
+
+export default function Train(){
+    return(
+        <p>Filler for now</p>
+    )
+}
