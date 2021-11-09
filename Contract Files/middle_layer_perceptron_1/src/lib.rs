45488fd0b8b30d635a51a6a34bb026b89174259e
vidur2 <vmod2005@gmail.com>
2021-11-09 11:38:59 -0500

    Gave capability to parse full csv

--- Contract Files/middle_layer_perceptron_1/src/lib.rs
+++ Contract Files/middle_layer_perceptron_1/src/lib.rs
@@ -34,7 +34,33 @@ pub trait PerceptronWeights{
 
 #[ext_contract(lower_level_neuron)]
 pub trait InputNeuron2{
-    fn adjust(&mut self, offset: f32, input1: f32, input2: f32, input3: f32, input4: f32, input5: f32);
+    fn adjust(
+        &mut self,
+        offset: f32,
+        input1: f32, 
+        input2: f32, 
+        input3: f32, 
+        input4: f32, 
+        input5: f32, 
+        input6: f32, 
+        input7: f32,
+        input8: f32,
+        input9: f32,
+        input10: f32,
+        input11: f32,
+        input12: f32,
+        input13: f32,
+        input14: f32,
+        input15: f32,
+        input16: f32,
+        input17: f32,
+        input18: f32,
+        input19: f32,
+        input20: f32,
+        input21: f32,
+        input22: f32,
+        input23: f32
+    );
 }
 
 #[near_bindgen]
@@ -61,7 +87,7 @@ impl MiddleNeuron{
         self.bias = self.bias + offset;
         let lower_level_neuron_account_id: AccountId = LOWER_LEVEL_NEURON_ID.to_string().trim().parse().expect("invalid");
         let gas_count = Gas::from(BASE_GAS * 13u64 - BASE_GAS * 5*8/4);
-        lower_level_neuron::adjust(offset, input_vector[0][0], input_vector[0][1], input_vector[0][2], input_vector[0][3], input_vector[0][4], lower_level_neuron_account_id, NO_DEPOSIT, gas_count);
+        lower_level_neuron::adjust(offset, input_vector[0][0], input_vector[0][1], input_vector[0][2], input_vector[0][3], input_vector[0][4], input_vector[0][5], input_vector[0][6], input_vector[0][7], input_vector[0][8], input_vector[0][9], input_vector[0][10], input_vector[0][11], input_vector[0][12], input_vector[0][13], input_vector[0][14],input_vector[0][15], input_vector[0][16], input_vector[0][17], input_vector[0][18], input_vector[0][19], input_vector[0][20], input_vector[0][21], input_vector[0][22], lower_level_neuron_account_id, NO_DEPOSIT, gas_count);
 
     }
     pub fn predict_raw(&self, input1: f32, input2: f32) -> near_sdk::Promise{
@@ -69,7 +95,7 @@ impl MiddleNeuron{
         let mut outputs = Vec::new();
         outputs.push(self.sigmoid(weighted_sum));
         let higher_level_neuron_account_id: AccountId = HIGHER_LEVEL_NEURON_ID.to_string().trim().parse().expect("invalid");
-        let gas_count = 0;
+        let gas_count = Gas::from(BASE_GAS * 19u64 - BASE_GAS * 5*2/4);
         higher_level_neuron::predict_raw(input1, input2, outputs, higher_level_neuron_account_id, NO_DEPOSIT, gas_count)
     }
     fn sigmoid(&self, input_sum: f32) -> f32{
