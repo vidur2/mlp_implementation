cd5be5bbd39f9843a5ee79a0f0305c799f9c4e47
vidur2 <vmod2005@gmail.com>
2021-12-18 12:49:26 -0500

    Updated Neuron Generator

--- Bigger Commercial Impl Example/blockchain_master_account/src/lib.rs
+++ Bigger Commercial Impl Example/blockchain_master_account/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{near_bindgen, env, AccountId, Promise, Balance};
+use near_sdk::{near_bindgen, env, AccountId, Promise };
 use std::collections::HashMap;
 
 const CODE: &[u8] = include_bytes!("../../general_blockchain_neuron.wasm");
@@ -12,23 +12,19 @@ pub struct NeuronGenerator{
 
 #[near_bindgen]
 impl NeuronGenerator{
-    #[payable]
     pub fn generate_mlp(&mut self, amt_neurons: u64){
-        let required_deposit: Balance = (amt_neurons as u128) * 250_000_000_000_000_000_000_000_000u128;
-        if env::attached_deposit() == required_deposit {
-            let account: String = env::signer_account_id().to_string();
-            self.account_ids.insert(account.clone(), amt_neurons);
-            for account_num in 1..amt_neurons{
-                let account_id: AccountId = format!("mlp{}.perceptron.{}.testnet", account_num, account)
-                    .parse()
-                    .expect("Invalid account id");
-                    
-                Promise::new(account_id)
-                    .create_account()
-                    .add_full_access_key(env::signer_account_pk())
-                    .transfer(250_000_000_000_000_000_000_000_000u128)
-                    .deploy_contract(CODE.to_vec());
-            }
+        let account: String = env::signer_account_id().to_string();
+        self.account_ids.insert(account.clone(), amt_neurons);
+        for account_num in 1..amt_neurons{
+            let account_id: AccountId = format!("mlp{}.perceptron.{}", account_num, account)
+                .parse()
+                .expect("Invalid account id");
+                
+            Promise::new(account_id)
+                .create_account()
+                .add_full_access_key(env::signer_account_pk())
+                .transfer(250_000_000_000_000_000_000_000_000u128)
+                .deploy_contract(CODE.to_vec());
         }
     }
 }
