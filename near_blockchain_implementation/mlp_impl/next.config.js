90a837e9a47a2a197e6551ad5e4d19dc61105585
vidur2 <vmod2005@gmail.com>
2022-01-14 22:08:26 -0500

    thing

--- near_blockchain_implementation/mlp_impl/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    env: {
-        private_key: "ed25519:5HcbEsGruC3pyJ4WdqJWVumb3NsoUQDyAfvDPqxLn3njqrgfMtM98zjXAYZ5fNWHz7c8uM6JLRz3VjcPbyrQP3sX",
-        nft_store_api_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGIwZDViYkMzQzgxQTJENUM1OTRGMDYyNTk0MTU1NzNGODk5ZWRiMjkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYyODgyMDc3MTc0MCwibmFtZSI6ImF1dG9ORlQifQ.0cYZwiubqYaBh3XN34ymg9RXgk0tJ4NJjvi-YNy204E"
-    }
-}
